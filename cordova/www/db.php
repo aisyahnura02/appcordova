f5c5d3082df80a9aa90074977476cb5671a3234e
aisyahnura02 <aisyahnura02@gmail.com>
2021-03-25 23:50:02 +0700

    projectapp

---
+++ cordova/www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id15157825_akadmk","f%[10AD%+B#(w+-x","id15157825_akademik2") or die ("could not connect database");
+?>
